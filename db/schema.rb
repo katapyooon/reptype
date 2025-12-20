2ba8b760418475560e010a8fa8d01ea5497e8931
片所宏介 <katashokousuke@katashokousukenoMacBook-Air.local>
2025-12-20 18:22:15 +0900

    questionテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 0) do
+ActiveRecord::Schema[8.0].define(version: 2025_12_20_091340) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
+  create_table "questions", force: :cascade do |t|
+    t.string "category"
+    t.string "content"
+    t.integer "position"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 end
