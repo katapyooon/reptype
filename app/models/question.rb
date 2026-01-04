18ed223eb344058f0b3864a3d28d2eb057999320
片所宏介 <katashokousuke@katashokousukenoMacBook-Air.local>
2026-01-04 17:20:12 +0900

    rubocop落ちそうだったので修正

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,5 +2,5 @@ class Question < ApplicationRecord
     has_many :answers, dependent: :destroy  # 追加: 質問削除時に回答も削除
 
     ## categoryのバリデーションを追加
-    validates :category, presence: true, inclusion: { in: ['A', 'B', 'C', 'D'] }
+    validates :category, presence: true, inclusion: { in: [ "A", "B", "C", "D" ] }
 end
